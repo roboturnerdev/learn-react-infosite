0a700e5d5db733baecbaee0af1b5413a80d41e36
Robert Turner <roboturnerdev@gmail.com>
2021-11-15 05:00:08 -0500

    final commit. small site finished looks drab but learned a lot

--- src/componentlib/Main.js
+++ src/componentlib/Main.js
@@ -2,6 +2,15 @@ import React from 'react';
 
 export default function Main() {
     return (
-        <h1>Main goes here</h1>
+        <main>
+            <h1 className="main--title">Fun facts about React</h1>
+            <ul className="main--ul">
+                <li>Released 2013</li>
+                <li>by Jordan Walke</li>
+                <li>100k stars on GH</li>
+                <li>maintained by Meta</li>
+                <li>powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
     )
 }
