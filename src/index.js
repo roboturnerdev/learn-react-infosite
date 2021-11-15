c175a96efd3e5d8667787f430d15edbb971fc9f9
Robert Turner <roboturnerdev@gmail.com>
2021-11-15 00:50:51 -0500

    finally got local react to behave with me now we can actually get started. reeeeeee

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import logo from './react.png';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootNode = document.getElementById("root");
+const reactLogo = (
+  <div>
+    <img alt="" src={logo} width="40px" />
+  </div>
+)
+
+ReactDOM.render(reactLogo, rootNode);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
