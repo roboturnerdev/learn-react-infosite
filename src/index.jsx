d08f2c8d22da583df30a17f317593d34db1af39e
Robert Turner <roboturnerdev@gmail.com>
2021-11-15 01:38:20 -0500

    callenge 3 complete

--- src/index.jsx
+++ src/index.jsx
@@ -1,42 +1,54 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import logo from './react.png';
 
 /**
-Challenge: 
+Challenge: (COMPLETE)
 
-Part 2: 
-- Add a `header` element with a nested `nav` element. Inside the `nav`,
-  include a `img` element with the image of the React logo inside
-  (src="./react-logo.png") and make sure to set the width to something
-  more manageable so it doesn't take up the whole screen
-- Add an `h1` with some text describing the page. (E.g. "Reasons
-  I'm excited to learn React"). Place it above the ordered list.
-- Add a `footer` after the list that says: 
-    "© 20xx <last name here> development. All rights reserved."
+- Move the `footer` into its own component called "Footer" 
+  and render that component inside the Page component.
+- Move the `h1` and `ol` together into another component
+  called "MainContent" and render inside Page as well.
  */
 
-function AlreadyKnow() {
+function Header() {
+  return (
+    <header>
+        <nav>
+          <img alt="" src={logo} width="65px" />
+        </nav>
+    </header>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>© 2021 roboturner development. All rights reserved.</small>
+    </footer>
+  )
+}
+
+function MainContent() {
   return (
     <div>
-      <header>
-        <nav>
-          <img alt="" src={logo} width="100px"></img>
-        </nav>
-        <h1>Am I going to be Awesome, Or am I already?</h1>
-      </header>
+      <h2>Am I going to be Awesome, Or am I already?</h2>
       <ol>
         <li>It's fast</li>
         <li>Maintained by Meta</li>
         <li>Meta is the FUTURE</li>
         <li>But really, if there is a future they are gonna be a huge part of it.</li>
       </ol>
-      <footer>
-        © 2021 roboturner development. All rights reserved.
-      </footer>
+    </div>
+  )
+}
+
+function AlreadyKnow() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   )
 };
@@ -44,26 +56,3 @@ function AlreadyKnow() {
 const root = document.getElementById('root');
 
 ReactDOM.render(<AlreadyKnow />, root);
-
-
-
-
-
-
-
-
-
-
-
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
