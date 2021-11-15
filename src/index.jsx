15c4afb5bd52243747f31109eff2a742daf7878a
Robert Turner <roboturnerdev@gmail.com>
2021-11-15 01:31:45 -0500

    challenge 2 complete

--- src/index.jsx
+++ src/index.jsx
@@ -5,16 +5,39 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import logo from './react.png';
 
+/**
+Challenge: 
 
+Part 2: 
+- Add a `header` element with a nested `nav` element. Inside the `nav`,
+  include a `img` element with the image of the React logo inside
+  (src="./react-logo.png") and make sure to set the width to something
+  more manageable so it doesn't take up the whole screen
+- Add an `h1` with some text describing the page. (E.g. "Reasons
+  I'm excited to learn React"). Place it above the ordered list.
+- Add a `footer` after the list that says: 
+    "© 20xx <last name here> development. All rights reserved."
+ */
 
 function AlreadyKnow() {
   return (
-    <ol>
-      <li>It's fast</li>
-      <li>Maintained by Meta</li>
-      <li>Meta is the FUTURE</li>
-      <li>But really, if there is a future they are gonna be a huge part of it.</li>
-    </ol>
+    <div>
+      <header>
+        <nav>
+          <img alt="" src={logo} width="100px"></img>
+        </nav>
+        <h1>Am I going to be Awesome, Or am I already?</h1>
+      </header>
+      <ol>
+        <li>It's fast</li>
+        <li>Maintained by Meta</li>
+        <li>Meta is the FUTURE</li>
+        <li>But really, if there is a future they are gonna be a huge part of it.</li>
+      </ol>
+      <footer>
+        © 2021 roboturner development. All rights reserved.
+      </footer>
+    </div>
   )
 };
 
