3ac386e946d3d2caa36d42b16c28895fffe64f18
Robert Turner <roboturnerdev@gmail.com>
2021-11-15 03:22:05 -0500

    fresh take new setup

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
+/**
+Challenge: Project setup
 
+- Create a `components` folder
+- Create the following components in separate files inside
+  the components folder.  In each one, just render an `h1` 
+  with the name of the component (e.g. return <h1>Navbar goes here</h1>):
+    - Navbar
+    - Main
+- Create an App component outside the components folder (sibling to
+  the index.js file)
+    - Have App render the Navbar and Main components
+- Import and render the App component inside of index.js using ReactDOM
+    - At this point you should have your "Navbar goes here" etc. showing up
+      in the mini-browser.
+- Go to Google fonts and get the "Inter" font with weights 400, 600, and 700.
+  Put the links to those fonts ABOVE the style.css link in index.html (Use
+  the `<link/>` elements instead of the @import or npm options for getting
+  the fonts. You may need to do some extra research to figure out how this 
+  works if you haven't done it before)
+*/
 // resources
-import './index.css';
+import './styles.css';
 
 // components
-import InfoSite from './componentlib/InfoSite';
+import App from './App';
 
-ReactDOM.render(<InfoSite />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
 
-// I think Webpack will help me organize and distribute
-// the dependencies. I can already tell there are going to be many
-// places requiring many things soon.
+
+// Notes:
 
 // I found a video about aggregate modules that helped understand import/export in es6.
 // Watch to the end for info about aggregate modules, aliases, and default exports.
